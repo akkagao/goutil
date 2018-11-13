287d728746f780c7f66c99a7c5df75c6eb1d18a9
高建文 <cmdgjw@hotmail.com>
2018-11-13 16:52:27 +0800

    增加日期格式化测试代码

--- date_test.go
+++ date_test.go
@@ -12,9 +12,18 @@ func Test_FormatTime(t *testing.T) {
 	Convey("格式化时间", t, func() {
 		now := time.Now()
 		timeStr := fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d", now.Year(), int(now.Month()), now.Day(), now.Hour(), now.Minute(), now.Second())
-		fmt.Println(timeStr)
 		Convey("测试正常情况", func() {
 			So(FormatTime(now), ShouldEqual, timeStr)
+		})
+	})
+}
+
+func Test_FormatDate(t *testing.T) {
+	Convey("格式化日期", t, func() {
+		now := time.Now()
+		dateStr := fmt.Sprintf("%d-%02d-%02d", now.Year(), int(now.Month()), now.Day())
+		Convey("格式化日期", func() {
+			So(FormatDate(now), ShouldEqual, dateStr)
 		})
 	})
 }
