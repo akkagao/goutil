6dbadcf43d48ffa900b2f5880e3833dff049d20e
高建文 <cmdgjw@hotmail.com>
2018-11-16 14:00:49 +0800

    检查err

--- sys.go
+++ sys.go
@@ -5,16 +5,25 @@ import (
 	"runtime"
 )
 
+/**
+* 获取当前方法的名称
+ */
 func GetMyName() string {
 	pc, _, _, _ := runtime.Caller(1)
 	return runtime.FuncForPC(pc).Name()
 }
 
+/**
+* 获取执行者的名称
+ */
 func GetExecutorName() string {
 	pc, _, _, _ := runtime.Caller(2)
 	return runtime.FuncForPC(pc).Name()
 }
 
+/**
+* 判断error是否为nil如果不为nil 输出执行者名称，跑出panic
+ */
 func ChkErr(err error) {
 	if err != nil {
 		log.Println("executor:", GetExecutorName())
